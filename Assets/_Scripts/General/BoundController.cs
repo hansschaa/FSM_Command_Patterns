27e0e46df179b7bac8cbd80c0ef49edbb3862382
Hans Schaa <hansschaadev@gmail.com>
2019-08-29 13:27:16 -0400

    Bounds for reset player position

---
+++ Assets/_Scripts/General/BoundController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class BoundController : MonoBehaviour
+{
+    public Transform resetPosition;
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        other.transform.position = resetPosition.position;
+    }
+}
